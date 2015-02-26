0861fa49a5315c7422c031808256a8bf7963f242
mingcc <lm90311@gmail.com>
2015-02-26 12:21:26 +0800

    add apt-get line

--- bin/init_ubuntu.sh
+++ bin/init_ubuntu.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
+sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
 
 if [ ! -e ~/.oh-my-zsh ]; then
     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    chsh -s /bin/zsh
 fi
+
+apt-get install tmux vim autojump curl ibus-mozc 
