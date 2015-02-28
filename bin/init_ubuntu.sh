4f35c00ef27aba0a1f7f0b7acb0732c234010f45
mingcc <lm90311@gmail.com>
2015-02-28 19:54:17 +0800

    add ack-grep symlink script

--- bin/init_ubuntu.sh
+++ bin/init_ubuntu.sh
@@ -7,6 +7,16 @@ if [ ! -e ~/.oh-my-zsh ]; then
     chsh -s /bin/zsh
 fi
 
-apt-get install tmux vim autojump curl ibus-mozc python-pip
+apt-get install tmux vim autojump curl ibus-mozc python-pip ack-grep
 
 pip install jedi virtualenvwrapper
+
+git clone https://github.com/junegunn/fzf ~/.fzf
+https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.02.12_amd64.deb
+
+
+if [ -e /usr/bin/ack-grep ]; then
+    echo "ack-grep is exists"
+    ln -s /usr/bin/ack-grep /usr/local/bin/ack
+    echo "created a symlink to /usr/local/bin/ack"
+fi
