434e64f9aa6c140004887e9e91ad1ead5707aea0
mingcc <lm90311@gmail.com>
2015-03-13 19:12:02 +0800

    add rules for samba

--- bin/iptables.sh
+++ bin/iptables.sh
@@ -16,4 +16,9 @@ iptables -P FORWARD ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -i $EXTIF -m state --state RELATED,ESTABLISHED -j ACCEPT
 
+# samba 
+iptables -A INPUT -i $EXTIF -p tcp -s 192.168.1.0/24 -m multiport --dport 139,445 -j ACCEPT
+
+iptables -A INPUT -i $EXTIF -p tcp -s 192.168.1.0/24 -m multiport --dport 137,138 -j ACCEPT
+
 #sed '13a /home/$USERNAME/bin/iptables.sh' /etc/rc.local
