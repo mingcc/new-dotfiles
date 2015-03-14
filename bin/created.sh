d1c30206248e383c5712e98282eb5db5c3ed871a
mingcc <lm90311@gmail.com>
2015-03-14 12:11:24 +0800

    more pretty print

--- bin/created.sh
+++ bin/created.sh
@@ -2,4 +2,9 @@
 
 
 
-awk '{FS=":"}  $3 >= 1000 { print "username: "$1 "\t\t " "uid: "$3 }' /etc/passwd
+awk 'BEGIN { print "~~~~~Users~~~~~~"} 
+    {FS=":"}  $3 >= 1000 { printf"username: %10s\tuid:%10s\n",$1,$3 }' /etc/passwd
+        
+
+awk 'BEGIN { print "~~~~~Groups~~~~~~"} 
+    {FS=":"}  $3 >= 1000 { printf"username: %10s\tgid:%10s\n",$1,$3 }' /etc/group
