282ac273e1d9854143c019f6c52e29fa489d6dff
mingcc <lm90311@gmail.com>
2015-03-16 12:30:11 +0800

    change wrong tcp to udp

--- bin/iptables.sh
+++ bin/iptables.sh
@@ -19,6 +19,6 @@ iptables -A INPUT -i $EXTIF -m state --state RELATED,ESTABLISHED -j ACCEPT
 # samba 
 iptables -A INPUT -i $EXTIF -p tcp -s 192.168.1.0/24 -m multiport --dport 139,445 -j ACCEPT
 
-iptables -A INPUT -i $EXTIF -p tcp -s 192.168.1.0/24 -m multiport --dport 137,138 -j ACCEPT
+iptables -A INPUT -i $EXTIF -p udp -s 192.168.1.0/24 -m multiport --dport 137,138 -j ACCEPT
 
 #sed '13a /home/$USERNAME/bin/iptables.sh' /etc/rc.local
