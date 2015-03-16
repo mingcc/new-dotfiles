a307ae34c5adf39d31808442126403c2e90b22aa
mingcc <lm90311@gmail.com>
2015-03-16 19:26:33 +0800

    add services section

--- bin/ubuntu.sh
+++ bin/ubuntu.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-HOME="/home/mingcc"
 
 apt-get update && sudo apt-get upgrade
 
 sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
 # sed -i '13a $HOME/bin/test' /etc/rc.local
+
+
+services
+========
+update-rc.d bluetooth stop
 
 if [ ! -e ~/.oh-my-zsh ]; then
     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
