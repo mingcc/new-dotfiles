69393fab88a275d390b03a3075427ab1d71903fa
mingcc <lm90311@gmail.com>
2015-03-24 12:26:10 +0800

    newest change_filename.sh

--- bin/change_filename.sh
+++ bin/change_filename.sh
@@ -2,8 +2,9 @@
 # Description: changes the filename such as,  27079_08_.sh to  instruder.sh.
 
 
-for file in $1; do
+for file in *.sh; do
     Filename=`cat $file | egrep '(^#Filename|^#Name)' | sed 's/\(#Filename:\|#Name:\)//g'`
     mv $1 $Filename
+    ls -l $Filename
 done
 
